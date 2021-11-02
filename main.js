257057f5a6c70601cab2dcefaee9b67644ff72fb
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-02 10:50:28 -0700

    while function

---
+++ main.js
@@ -0,0 +1,62 @@
+// var i=1;
+
+// while (i>10){
+//     console.log(i);
+//     i++;
+// }
+//
+// do {
+//     console.log(i);
+//     i++;
+// }while (i>10);
+// var n=parseInt(prompt("n="))
+// var k=0;
+// var res=0;
+// while (true){
+//     k++;
+//  res+=k;
+//  if (res>=n){
+//      break;
+//  }
+//
+// }
+//     alert(k);
+// alert(res);
+
+// 19-misol
+// var n=parseInt(prompt("n="));
+// var i=1;
+// var s=0;
+// while (parseInt(n/i)>=1){
+//   var son=parseInt(n/i)%10;
+//   s+=son;
+//   i*=10;
+// }alert(s);
+
+// var n=parseInt(prompt("n="));
+// var a=[];
+// var item=1;
+// for (var i=0; i<n; i++){
+//     a[i]=item;
+//     item+=2;
+// }
+//
+// for (let i = 0; i < a.length ; i++) {
+//     console.log(a[i])
+// }
+
+// var n=parseInt(prompt("n="));
+// var a=[];
+// for (let i = 0; i <n ; i++) {
+//     a[i]=parseInt(prompt(i+"-son"));
+// }
+// for (let i = a.length-1; i >=0 ; i--) {
+//     console.log(a[i])
+// }
+
+// var n1=parseInt(prompt("n1="));
+// var n2=parseInt(prompt("n2="));
+// var s1=prompt("s1=");
+// var s2=prompt("s2=");
+// var res=s1.slice(0,n1)+s2.slice(s2.length-n2);
+// alert(res);
